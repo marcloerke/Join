93e18ccf9356592db8a6faa2dc935e773261683c
Marc Hendrik Loerke <marc.loerke@gmail.com>
2023-01-11 14:03:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 async function init() {
     await includeHTML();
-    document.getElementById('headline').innerHTML = 'Herzlich willkommen!';
+    document.getElementById('headline').innerHTML = 'Herzlich willkommen!'; // Test for await-template-function
 }
 
 async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
         const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // "includes/header.html"
+        file = element.getAttribute("w3-include-html"); // "assets/templates/desktop_template.html"
         let resp = await fetch(file);
         if (resp.ok) {
             element.innerHTML = await resp.text();
