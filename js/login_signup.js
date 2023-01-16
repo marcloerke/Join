e30c786095a4ef6a2d870f8b9c966b363ae1d6fe
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-01-16 17:44:00 +0100

    added forgot password feature
    added forgot password feature  + minor fixes regarding error message handling

--- js/login_signup.js
+++ js/login_signup.js
@@ -1,19 +1,17 @@
 //script variables
 
 let cardContainer = document.getElementById("card-container");
-let header= document.getElementsByTagName("header")[0];
-let message;
+let header = document.getElementsByTagName("header")[0];
+let signupSuccess= false;
 setURL("https://gruppe-428.developerakademie.net/smallest_backend_ever");
 
 /**
  * Main functions
  */
-function  init() {
-  setTimeout(function (){
+function init() {
+  setTimeout(function () {
     renderLogin();
-    
-  }
-  , 800)
+  }, 800);
 }
 
 /**
@@ -21,40 +19,111 @@ function  init() {
  */
 
 function renderSignUpWrapper() {
-  let signUpWrapper= document.createElement('div');
-  signUpWrapper.classList.add('signup-wrapper');
-  signUpWrapper.innerHTML= headerTemplate();
+  let signUpWrapper = document.createElement("div");
+  signUpWrapper.classList.add("signup-wrapper");
+  signUpWrapper.innerHTML = headerTemplate();
   header.append(signUpWrapper);
 }
 
-
 function renderLogin() {
   cardContainer.innerHTML = loginCardTemplate();
-  let loginForm = document.querySelector("#login-form");
-  loginForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-  });
-  loginForm.addEventListener("submit", function () {
-    login();
-  });
+  if (localStorage.length > 0) {
+    let password = document.getElementById("login-password");
+    let email = document.getElementById("login-mail");
+    password.value= localStorage.getItem('password');
+    email.value= localStorage.getItem('email');
+  }
+  addLoginEventListeners();
+  if (signupSuccess) {
+    let success= document.getElementById("success");
+    success.innerText= "Signup successful!";
+  }
   renderSignUpWrapper();
 }
 
 function renderSignUp() {
+  signupSuccess= false;
   let signUpWrapper = document.getElementsByClassName("signup-wrapper")[0];
-  signUpWrapper.style.display= "none";
+  signUpWrapper.remove();
   cardContainer.innerHTML = signUpCardTemplate();
+  addSignupEventListeners();
+}
+
+function renderLoginDeny() {
+  document.getElementById("invalid").innerText = "Invalid Username or Password";
+}
+
+function renderNoUser() {
+  document.getElementById('noUser').innerText="E-Mail does not exist in database.";
+}
+
+function generateTooltip(e) {
+  let message= "This field is required!";
+  let inputField= e.target;
+  if(inputField.type === 'email' && inputField.value.length > 0) {
+    message= "This field must contain an @- Symbol"
+  }
+
+  let tooltip = document.createElement("div");
+  tooltip.classList.add("validation-tooltip");
+  tooltip.innerHTML = /*html*/ `
+        <p>!</p>
+        <p>${message}</p>
+        `;
+  e.target.parentNode.append(tooltip);
+}
+
+function renderForgotPassword() {
+  cardContainer.innerHTML= forgotPasswordTemplate();
+  let signUpWrapper = document.getElementsByClassName("signup-wrapper")[0];
+  signUpWrapper.remove();
+  addForgotPasswordEventListener();
+}
+
+/**
+ * adding event listeners
+ */
+
+function addLoginEventListeners() {
+  let loginForm = document.querySelector("#login-form");
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    login();
+  });
+  let guestLoginButton= document.querySelector('#guestLogin');
+  guestLoginButton.addEventListener('click',function(event){
+    event.preventDefault();
+    guestLogin();
+  })
+}
+
+function addSignupEventListeners() {
   let signupForm = document.querySelector("#signup-form");
   signupForm.addEventListener("submit", function (event) {
     event.preventDefault();
-  });
-  signupForm.addEventListener("submit", function () {
     signUp();
   });
 }
 
+function addForgotPasswordEventListener() {
+  let forgotPasswordForm= document.getElementById('forgot-password-form');
+  forgotPasswordForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    sendPasswordReset();
+  });
+}
+
+
+window.addEventListener("click", function () {
+  removeLoginDeny();
+  removeSignUpSuccess();
+  removeValidationTooltip();
+  removeNoUser();
+});
+
+
 /**
- * sign up/ log in
+ * sign up/ log in/ password reset
  */
 
 async function signUp() {
@@ -70,7 +139,7 @@ async function signUp() {
   };
   users.push(newUser);
   await backend.setItem("users", JSON.stringify(users));
-  alert('Signup successful!');
+  signupSuccess= true;
   renderLogin();
 }
 
@@ -83,33 +152,78 @@ async function login() {
     return u.password == password.value && u.email == email.value;
   });
   if (user) {
-    window.location.href= "index.html";
-  } 
-  else {
+    let checkbox= document.getElementById('checkbox');
+    if(checkbox.checked) {
+      localStorage.setItem('password', password.value);
+      localStorage.setItem('email', email.value);
+    }
+    window.location.href = "index.html";
+  } else {
     renderLoginDeny();
   }
 }
 
-function renderLoginDeny() {
-  document.getElementById('invalid').innerText="Invalid Username or Password";
+
+function guestLogin() {
+  let password = document.getElementById("login-password");
+  let email = document.getElementById("login-mail");
+  password.value= 'guest';
+  email.value= 'guest@mail.de';
+  setTimeout(function () {
+    window.location.href = "index.html";
+  }, 250)
 }
+
+
+async function sendPasswordReset() {
+  let passwordResetMail= document.querySelector('#password-reset-mail');
+  await downloadFromServer();
+  users = JSON.parse(backend.getItem("users")) || [];
+  let user = users.find((u) => {
+    return u.email == passwordResetMail.value;
+  });
+  if(user) {
+    alert('Password reset E-Mail sent!');
+    renderLogin();
+  }
+  else {
+    renderNoUser();
+  }
+}
+
+
+/**
+ * removing temporary elements from document
+ */
+
+function removeNoUser() {
+  let noUser = document.getElementById("noUser");
+  if (noUser) {
+    noUser.innerText = "";
+  }
+}
+
 
 function removeLoginDeny() {
-  let errorMessage= document.getElementById('invalid');
-  if (errorMessage){
-    errorMessage.innerText='';
+  let errorMessage = document.getElementById("invalid");
+  if (errorMessage) {
+    errorMessage.innerText = "";
   }
-
 }
 
+function removeSignUpSuccess() {
+  let successMessage = document.getElementById("success");
+  if (successMessage) {
+    successMessage.innerText = "";
+  }
+}
 
-window.addEventListener('click', function () {
-  removeLoginDeny();
-})
-
-
-
-
+function removeValidationTooltip() {
+  let tooltips= document.getElementsByClassName('validation-tooltip');
+  if(tooltips) {
+    [...tooltips].forEach(element => {element.remove()})
+  }
+}
 
 //template functions
 
@@ -117,36 +231,38 @@ function headerTemplate() {
   return /*html*/ `
         <p>Not a join user?</p>
         <button onclick="renderSignUp()">Sign up</button>
-  `
+  `;
 }
 
 function loginCardTemplate() {
   return /*html*/ `
         
   
-        <form class='login-card' id="login-form">
+        <form class='form-card' id="login-form">
           <h1>Log in</h1>
+          <img src="assets/img/icon_line.png" class="line">
           <div class='user-input'>
-            <input id='login-mail' type='email' placeholder='Email' required />
+            <input id='login-mail' type='email' placeholder='Email' required oninvalid="event.preventDefault(); generateTooltip(event)"/>
             <img src='assets/img/icon_mail.png' />
           </div>
           <div class='user-input'>
             <p id="invalid"></p>
-            <input id='login-password' type='password' placeholder='Password' required />
-            <img src='assets/img/icon_mail.png' />
+            <p id="success"></p>
+            <input id='login-password' type='password' placeholder='Password' required oninvalid="event.preventDefault(); generateTooltip(event)"/>
+            <img src='assets/img/icon_lock.png' />
           </div>
           
           <div class='login-options-container'>
             <div class='column1'>
               <div>
-                <input type='checkbox' />
+                <input type='checkbox' id="checkbox"/>
                 <p>Remember me</p>
               </div>
               <button>Log in</button>
             </div>
             <div class='column2'>
-              <p>Forgot my password</p>
-              <button>Guest Log in</button>
+              <p onclick="renderForgotPassword()">Forgot my password</p>
+              <button id="guestLogin">Guest Log in</button>
             </div>
           </div>
         </form>
@@ -155,22 +271,43 @@ function loginCardTemplate() {
 
 function signUpCardTemplate() {
   return /*html*/ `
-     <form class='login-card' id="signup-form">
+     <form class='form-card' id="signup-form">
       <img src='assets/img/icon_back.png' alt='arrow' class='go-back' onclick='renderLogin()'>
           <h1>Sign Up</h1>
+          <img src="assets/img/icon_line.png" class="line">
           <div class='user-input'>
-            <input id='signup-name' type='text' placeholder='Name' required />
+            <input id='signup-name' type='text' placeholder='Name' required oninvalid="event.preventDefault(); generateTooltip(event)"/>
+            <img src='assets/img/icon_profile.png' />
+          </div>
+          <div class='user-input'>
+            <input id='signup-mail' type='email' placeholder='Email' required oninvalid="event.preventDefault(); generateTooltip(event)"/>
             <img src='assets/img/icon_mail.png' />
           </div>
           <div class='user-input'>
-            <input id='signup-mail' type='email' placeholder='Email' required />
-            <img src='assets/img/icon_mail.png' />
-          </div>
-          <div class='user-input'>
-            <input id='signup-password' type='password' placeholder='Password' required />
-            <img src='assets/img/icon_mail.png' />
+            <input id='signup-password' type='password' placeholder='Password' required oninvalid="event.preventDefault(); generateTooltip(event)"/>
+            <img src='assets/img/icon_lock.png' />
           </div>
           <button class='sign-up'>Sign up</button>
       </form>
   `;
 }
+
+function forgotPasswordTemplate() {
+  return /*html*/ `
+    <form class='form-card' id="forgot-password-form">
+      <img src='assets/img/icon_back.png' alt='arrow' class='go-back' onclick='renderLogin()'>
+          <h1>Reset password</h1>
+          <img src="assets/img/icon_line.png" class="line">
+          <h4>In order to reset your password enter account E-Mail below in order to receive instructions on how to change your password.</h4>
+          <div class='user-input'>
+            <input id='password-reset-mail' type='email' placeholder='Email' required oninvalid="event.preventDefault(); generateTooltip(event)"/>
+            <img src='assets/img/icon_mail.png' />
+            <p id="noUser"></p>
+          </div>
+          <button class='sign-up'>Send Email</button>
+      </form>
+  `
+}
+
+
+
