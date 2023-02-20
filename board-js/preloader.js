ccfe0dd3edda734c8001fa07a1438428e0eed77b
Barnabas Gonda <barnabasgonda@Barnabass-iMac.local>
2023-02-20 21:05:16 +0100

    last commit

--- board-js/preloader.js
+++ board-js/preloader.js
@@ -2,20 +2,14 @@
 
 setURL('https://gruppe-428.developerakademie.net/smallest_backend_ever');
 
-let contacts= [];
+let contacts = [];
 
 async function init() {
     await downloadFromServer();
-    //backend.deleteItem('keyTasks');
-    //backend.deleteItem('keyContactList');
-    tasks = JSON.parse(backend.getItem('keyTasks')) || []; 
+    tasks = JSON.parse(backend.getItem('keyTasks')) || [];
     contacts = JSON.parse(backend.getItem("contacts")) || [];
-    //contactList = JSON.parse(backend.getItem('keyContacts')) || [];
     await includeHTML();
-    //console.log('contactList nach dem Laden: ',contactList);
     updateHTML();
-    // console.log('tasks nach dem Laden: ',tasks);
-    //console.log('contactList nach dem Laden: ',contactList);
 }
 
 init();
