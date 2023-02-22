b61e8f2e414c3ca4c815e202c62b797cd9efbcae
MoritzGeorgy95 <104759604+MoritzGeorgy95@users.noreply.github.com>
2023-02-22 16:56:29 +0100

    task overlay

--- js/contacts.js
+++ js/contacts.js
@@ -170,7 +170,8 @@ function formValidation() {
   let inputs = document.getElementsByTagName("input");
   let userPattern = /^[0-9]*[a-zA-Z]{2,}.*$/;
   let mailPattern = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
-  let phonePattern = /^\+\d{1,}\s?\d{1,}\s?\d{1,}\s?\d{1,}\s?\d{1,}\s?\d{1,}\s?\d{1,}$/;
+  let phonePattern =
+    /^\+\d{1,}\s?\d{1,}\s?\d{1,}\s?\d{1,}\s?\d{1,}\s?\d{1,}\s?\d{1,}$/;
   let allCorrect = true;
 
   for (let i = 0; i < inputs.length; i++) {
@@ -182,32 +183,41 @@ function formValidation() {
       generateTooltip(input, message);
     }
 
-    if (input.type === "email" && !mailPattern.test(input.value) && input.value.length > 0) {
+    if (
+      input.type === "email" &&
+      !mailPattern.test(input.value) &&
+      input.value.length > 0
+    ) {
       let message = "Please enter a valid email adress!";
       allCorrect = false;
       generateTooltip(input, message);
     }
 
-    if (input.type === "tel" && !phonePattern.test(input.value) && input.value.length > 0) {
+    if (
+      input.type === "tel" &&
+      !phonePattern.test(input.value) &&
+      input.value.length > 0
+    ) {
       let message = "Please enter a valid phone number!";
       allCorrect = false;
       generateTooltip(input, message);
     }
 
-    if (input.type === "text" && !userPattern.test(input.value) && input.value.length > 0) {
+    if (
+      input.type === "text" &&
+      !userPattern.test(input.value) &&
+      input.value.length > 0
+    ) {
       let message = "Please enter a valid name!";
       allCorrect = false;
       generateTooltip(input, message);
     }
-
-    
   }
   // console.log(allCorrect);
-  return allCorrect
+  return allCorrect;
 }
 
 function generateTooltip(input, message) {
-  
   let tooltip = document.createElement("div");
   tooltip.classList.add("validation-tooltip");
   tooltip.innerHTML = /*html*/ `
@@ -314,7 +324,7 @@ const showContactData = (contact) => {
             <div class="initials-big" style="background: ${contact.color}">${initials}</div>
             <div class="add-task-container-small">
               <h1>${contact["userName"]}</h1>
-              <div class="contact-task">
+              <div class="contact-task" id="contact-task">
                   <img src="assets/img/icon_add_task_plus.png" alt="#">
                   <h2>Add Task</h2>
               </div>
@@ -411,10 +421,107 @@ function removeValidationTooltip() {
   }
 }
 
-window.addEventListener('click', function(event){
-  let button= this.document.getElementById('requireFill');
+window.addEventListener("click", function (event) {
+  let button = this.document.getElementById("requireFill");
   if (event.target != button) {
     removeValidationTooltip();
   }
+});
 
-})
+//assign contact to new task
+
+function addTaskTemplate() {
+  return /*html*/ `
+
+<div id="layover" onclick="closeAddTask()" class="layover ">
+    <div onclick="stopPropagation(event)" class="popup-container">
+
+      <div id="taskCard" class="task-card">
+        <h1>Add Task</h1>
+        <div class="column-container form">
+          <div class="column-left">
+            <label for="title">Title</label>
+            <input type="text" id="title" placeholder="Enter a title" />
+            <p class="required"></p>
+
+            <label for="name" class="mt-15">Description</label>
+            <textarea id="description" placeholder="Enter a description"></textarea>
+            <p class="required"></p>
+            <label class="mt-15" for="selectCategory">Category</label>
+            <div class="category-input-container">
+              <input readonly id="selectCategory" value="Select task category"></input>
+
+              <img src="assets/img/icon_triangle.png" id="addCategoryIcon">
+              <div id="categoryDot"></div>
+              <div id="categoryOninput">
+                <img src="assets/img/icon_clear.png" id="clearCategory">
+                <img src="assets/img/icon_done.png" id="addCategory">
+              </div>
+            </div>
+
+            <div id="dropdownContainer"></div>
+            <div id="colorContainer">
+              <div class="color-dot orange"></div>
+              <div class="color-dot red"></div>
+              <div class="color-dot blue"></div>
+              <div class="color-dot lightblue"></div>
+              <div class="color-dot pink"></div>
+              <div class="color-dot lightpink"></div>
+              <div class="color-dot green"></div>
+              <div class="color-dot turquo"></div>
+            </div>
+            <p class="required"></p>
+
+            <label for="assignedTo">Assigned to</label>
+            <div class="contact-input-container">
+              <input readonly id="assignedTo" value="Select contacts to assign"></input>
+              <img src="assets/img/icon_triangle.png" id="addContactIcon">
+            </div>
+            <div id="contactsDropdownContainer"></div>
+          </div>
+          <div class="column-right">
+            <label for="date">Due Date</label>
+            <input type="date" id="date" min="">
+            <p class="required"></p>
+            <label class="mt-15" for="prio">Prio</label>
+            <div id="prio" class="prio">
+              <div class="prio-btn">Urgent
+                <img src="assets/img/icon_up.png" alt="sda">
+              </div>
+              <div class="prio-btn">Medium
+                <img src="assets/img/icon_medium.png" alt="sda">
+              </div>
+              <div class="prio-btn">Low
+                <img src="assets/img/icon_down.png" alt="sda">
+              </div>
+            </div>
+            <p class="required"></p>
+            <label for="addSubtask" class="mt-15">Subtasks</label>
+            <div class="subtask-input-container">
+              <input type="text" id="addSubtask">
+              <img src="assets/img/icon_plus.png" id="addSubtaskIcon">
+              <div id="subtaskOninput">
+                <img src="assets/img/icon_clear.png" id="clearSubtaskInput">
+                <img src="assets/img/icon_done.png" id="finishEditingSubtask">
+              </div>
+            </div>
+            <div id="subtaskContainer"></div>
+            <div class="action-button-container">
+              <button id="clear">
+                Clear
+                <img src="assets/img/icon_close.png">
+              </button>
+              <button id="create">
+                Create Task
+                <img src="assets/img/icon_create.png">
+              </button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  `;
+}
