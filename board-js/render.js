5a03d9ad7637310b4b82370ec2d89edf694a8940
Barnabas Gonda <barnabasgonda@Barnabass-iMac.local>
2023-02-22 19:35:54 +0100

    css update for buttons

--- board-js/render.js
+++ board-js/render.js
@@ -67,8 +67,12 @@ function renderLayoverTaskPopup(id) {
     <div id="contentTaskPopup${id}" onclick="stopPropagation(event)" class="content-task-popup d-none "></div>
     <div id="editContainerWrapper${id}" onclick="stopPropagation(event)"class="edit-container-wrapper d-none">
         <div id="editContainer${id}" class="edit-container " onclick="stopPropagation(event)"></div>
-        <img id="okButtonBlack${id}" onmouseenter="okButtonToBlue(${id})" class="ok-button"src="assets/img/ok-button-black.svg">
-        <img id="okButtonBlue${id}" onclick="editFinish(${id})" onmouseleave="okButtonToBlack(${id})"class="ok-button d-none" src="assets/img/ok-button-blue.svg">
+        <svg  class="ok-button"  onclick="editFinish(${id})" width="85" height="51" viewBox="0 0 85 51" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect width="85" height="51" rx="10" fill="#2A3647"/>
+<path d="M25.3622 25.3636C25.3622 27.0291 25.0465 28.446 24.4151 29.6143C23.7887 30.7827 22.9336 31.6751 21.8498 32.2915C20.771 32.9031 19.5579 33.2088 18.2106 33.2088C16.8533 33.2088 15.6353 32.9006 14.5565 32.2841C13.4776 31.6676 12.625 30.7752 11.9986 29.6069C11.3722 28.4386 11.0589 27.0241 11.0589 25.3636C11.0589 23.6982 11.3722 22.2812 11.9986 21.1129C12.625 19.9446 13.4776 19.0547 14.5565 18.4432C15.6353 17.8267 16.8533 17.5185 18.2106 17.5185C19.5579 17.5185 20.771 17.8267 21.8498 18.4432C22.9336 19.0547 23.7887 19.9446 24.4151 21.1129C25.0465 22.2812 25.3622 23.6982 25.3622 25.3636ZM22.0884 25.3636C22.0884 24.2848 21.9268 23.375 21.6037 22.6342C21.2855 21.8935 20.8356 21.3317 20.2539 20.9489C19.6722 20.5661 18.9911 20.3746 18.2106 20.3746C17.43 20.3746 16.7489 20.5661 16.1673 20.9489C15.5856 21.3317 15.1332 21.8935 14.81 22.6342C14.4918 23.375 14.3327 24.2848 14.3327 25.3636C14.3327 26.4425 14.4918 27.3523 14.81 28.093C15.1332 28.8338 15.5856 29.3956 16.1673 29.7784C16.7489 30.1612 17.43 30.3526 18.2106 30.3526C18.9911 30.3526 19.6722 30.1612 20.2539 29.7784C20.8356 29.3956 21.2855 28.8338 21.6037 28.093C21.9268 27.3523 22.0884 26.4425 22.0884 25.3636ZM27.7542 33V17.7273H30.9832V24.4613H31.1846L36.6807 17.7273H40.551L34.8834 24.5657L40.6182 33H36.7552L32.5716 26.7209L30.9832 28.6598V33H27.7542Z" fill="white"/>
+<path d="M55 25.5L61 31.5L71 19.5" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
     </div>`;
 }
 
@@ -90,8 +94,12 @@ function renderTaskPopup(id, backgroundColorCategory, taskCategory, taskTitle, d
     </div>
     <div id="priorityContainer${id}" class="priority-container set-fonts"></div>
     <div id="assignedTo${id}" class="assigned-to set-fonts">Assigned To:</div>
-    <img id="editButtonDark${id}"  onmouseenter="editButtonBlue(${id})" class="edit-button-dark" src="assets/img/edit-button-dark.svg">
-    <img id="editButtonBlue${id}" onclick="editTask(${id})" onmouseleave="editButtonDark(${id})"class="edit-button-blue d-none" src="assets/img/edit-button-blue.svg">   `;
+    <svg class="edit-button" onclick="editTask(${id})" width="57" height="57" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect class="edit-button" width="57" height="57" rx="10" fill="#2A3647"/>
+<path d="M20.9449 35.5155L25.7643 38.4404L38.4074 17.6083C38.694 17.1362 38.5435 16.5211 38.0714 16.2346L34.9618 14.3474C34.4897 14.0608 33.8746 14.2113 33.5881 14.6834L20.9449 35.5155Z" fill="white"/>
+<path d="M20.3599 36.4792L25.1792 39.4041L20.4506 41.6889L20.3599 36.4792Z" fill="white"/>
+</svg>
+      `;
 }
 
 
